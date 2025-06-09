0181a1a75bd2a10b924af1b371c38d34948ecbda
BBTibbers <youngsuk0311@gmail.com>
2025-06-09 10:51:14 +0900

    Initial commit

---
+++ Assets/Script/LinqTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LinqTest : MonoBehaviour
+{
+    private void Start()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student("Alice", 20, "A"),
+            new Student("Bob", 22, "B"),
+            new Student("Charlie", 21, "C"),
+            new Student("David", 23, "A")
+        };
+    }
+}
---
+++ Assets/Script/Student.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Student 
+{
+    public string Name;
+    public int Age;
+    public string Grade;
+    public Student(string name, int age, string grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+    public void DisplayInfo()
+    {
+        Debug.Log($"Name: {Name}, Age: {Age}, Grade: {Grade}");
+    }
+
+}
